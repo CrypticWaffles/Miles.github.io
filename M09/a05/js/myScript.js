2fa89abeb7e05fa6a61b2d2616153e073e53956d
Miles <90667845+CrypticWaffles@users.noreply.github.com>
2023-06-01 15:19:09 -0700

    Update myScript.js

--- M09/a05/js/myScript.js
+++ M09/a05/js/myScript.js
@@ -1,1 +1,26 @@
+var index = 1;
+showSlides(index);
 
+function plusSlides(n) {
+  showSlides(index += n);
+}
+
+function currentSlide(n) {
+  showSlides(index = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {index = 1;}
+  if (n < 1) {index = slides.length;}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[index-1].style.display = "block";
+}
+
+setInterval(function () {plusSlides(1);}, 5000);
+
+document.getElementById("next").onclick = function() { plusSlides(1); };
+document.getElementById("prev").onclick = function() { plusSlides(-1); };
